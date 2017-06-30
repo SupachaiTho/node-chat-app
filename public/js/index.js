0a6a368905c9d408d7cb2aab2c3d2570a784a4b6
supachai.thongprasit <supachai.thongprasit@gmail.com>
2017-06-30 19:31:09 +0700

    Format timestam with js

--- public/js/index.js
+++ public/js/index.js
@@ -8,19 +8,21 @@ socket.on('disconnect',function(){
 });
 
 socket.on('newMessage',function (message){
-    console.log('newMessage',message)
+    var formattedTime = moment(message.createdAt).format('h:mm a')
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`)
+    li.text(`${message.from} ${formattedTime}: ${message.text}`)
     jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit',function(e){
     e.preventDefault();
 
+    var formattedTime = moment().format('h:mm a')
+
     var messageTextbox =  jQuery('[name=message]')
 
     var li = jQuery('<li></li>');
-    li.text(`Me: ${messageTextbox.val()}`)
+    li.text(`Me ${formattedTime}: ${messageTextbox.val()}`)
     jQuery('#messages').append(li);
     socket.emit('createMessage',{
         from:'User',
@@ -33,8 +35,9 @@ jQuery('#message-form').on('submit',function(e){
 socket.on('newLocationMessage',function (message){
     var li = jQuery('<li></li>')
     var a = jQuery('<a target="_blank" >My current location</a>');
+    var formattedTime = moment(message.createdAt).format('h:mm a')
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a)
     jQuery('#messages').append(li);
