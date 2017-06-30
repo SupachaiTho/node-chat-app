04fd5a4a32a8b8dfd0fd9e9ae70dcc8639bd1c20
supachai.thongprasit <supachai.thongprasit@gmail.com>
2017-06-30 18:45:22 +0700

    Add ip

--- server/server.js
+++ server/server.js
@@ -19,19 +19,19 @@ io.on('connection',(socket)=>{
     console.log('New User Connected')
 
     var socketId = socket.id;
-    var clientIp = socket.request.connection.remoteAddress;
+    var clientIpAddress = socket.request.headers['x-forwarded-for'] || socket.request.connection.remoteAddress;
 
     socket.emit('newMessage',generateMessage('Admin','Welcome to the Chat App'))
 
     socket.broadcast.emit('newMessage',generateMessage('Admin','New user jointed'))
 
     socket.on('createMessage',(message, callback)=>{
-        socket.broadcast.emit('newMessage',generateMessage(clientIp,message.text))
+        socket.broadcast.emit('newMessage',generateMessage(clientIpAddress,message.text))
         callback('This from server');
     })
 
     socket.on('createLocationMessage',(coords)=>{
-        io.emit('newLocationMessage',generateLocationMessage(clientIp,coords.latitude,coords.longitude))
+        io.emit('newLocationMessage',generateLocationMessage(clientIpAddress,coords.latitude,coords.longitude))
     })
 
     socket.on('disconnect',()=>{
